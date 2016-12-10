ce5dfa39edeccd7f0f0ce0df3f5fa52f8eca41c1
tkalmi <tomi.kalmi@gmail.com>
2016-12-10 21:11:21 +0200

    Fix minor typo in instructions

--- client/components/Instructions.jsx
+++ client/components/Instructions.jsx
@@ -7,7 +7,7 @@ const Instructions = ({ dismissInstructions }) => (
             <i className="fa fa-times" aria-hidden="true" onClick={dismissInstructions}></i>
             <p>Luo uusi kauppalista painamalla keskellä olevaa nappia "Luo uusi lista". Tällöin sinut ohjataan uudelle sivulle, jossa aukeaa uusi kauppalistasi. Pääset käyttämään listaasi myös myöhemmin siirtymällä kauppalistan URLiin.</p>
             <p>Lisää uusi tavara kauppalistaan täyttämällä sen nimi ja painamalla "Lisää"-nappia.</p>
-            <p>Voit muokata listassa olevaa tavaroa painamalla kynän kuvaa.</p>
+            <p>Voit muokata listassa olevaa tavaraa painamalla kynän kuvaa.</p>
             <p>Voit poistaa listassa olevan tavaran painamalla roskakorin kuvaa.</p>
             <p>Voit merkata listassa olevan tavaran ostetuksi painamalla oikeinmerkkiä. Kun painat "Kauppareissu päättynyt"-painiketta, listasta poistetaan kaikki ostetuksi merkatut tavarat.</p>
         </article>
